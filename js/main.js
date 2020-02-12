7f25e17243699cc94a2608dad19837645070ec0d
LAbeast021 <kiarashhesampour@gmail.com>
2020-02-12 13:29:11 -0800

    information

--- js/main.js
+++ js/main.js
@@ -1,10 +1,14 @@
-const minesweeper;
-const carsjr;
-const zart;
-const nova;
+// const minesweeper;
+// const carsjr;
+// const zart;
+// const nova;
 
 
-$('.arrow').on('click',function(evt){
-    console.log(evt.target.name)
-    $(`#${evt.target.name}-pic`).attr('src','img/carsjr menue.png')
-})
+// $('.arrow').on('click',function(evt){
+//     console.log(evt.target.name)
+//     $(`#${evt.target.name}-pic`).attr('src','img/carsjr menue.png')
+// })
+
+// $('.link').on('click', function(evt){
+//     console.log(evt.target.text)
+// })
